5590fe115357872f4626c31c1997d4dddf277a31
Andre <opolloandres@gmail.com>
2023-01-16 11:48:47 +0300

    Added tab layout

--- app/src/main/java/com/example/tablayout/MainActivity.kt
+++ app/src/main/java/com/example/tablayout/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.tablayout
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,8 +24,26 @@ class MainActivity : AppCompatActivity() {
             R.drawable.image7
         )
     val viewPager = findViewById<ViewPager2>(R.id.viewpager)
+        val tabLayout = findViewById<TabLayout>(R.id.tablayout)
         val adapter = ViewPagerAdapter(images)
         viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout,viewPager){tab,position->
+            tab.text="Tab ${position + 1}"
+        }.attach()
+      tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+          override fun onTabSelected(tab: TabLayout.Tab?) {
+              Toast.makeText(this@MainActivity,"Selected ${tab?.text}", Toast.LENGTH_SHORT).show()
+          }
+
+          override fun onTabUnselected(tab: TabLayout.Tab?) {
+             Toast.makeText(this@MainActivity,"Unselected ${tab?.text}",Toast.LENGTH_SHORT).show()
+          }
+
+          override fun onTabReselected(tab: TabLayout.Tab?) {
+            Toast.makeText(this@MainActivity,"Reselected ${tab?.text}",Toast.LENGTH_SHORT).show()
+          }
+      })
     }
 
 }
